3a538685063b11df36190baf6973f14c7122bd15
Fabian Maisberger <fabian.maisberger@googlemail.com>
2019-08-04 12:28:47 +0200

    OSM im Dimple Keymap \n changed tap_fabi so it only loads when tapdance is enabled /some changes in configs

--- keyboards/lazydesigners/dimple/keymaps/fabian0520/keymap.c
+++ keyboards/lazydesigners/dimple/keymaps/fabian0520/keymap.c
@@ -38,7 +38,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
  [_COLEMAK] =LAYOUT( \
         KC_ESC ,   KC_Q   ,   KC_W   ,   KC_F   ,   KC_P   ,   KC_G   ,   KC_J   ,   KC_L   ,   KC_U   ,   KC_Y   ,  KC_SCLN , KC_BSPC  ,\
         LT(_SYM,KC_TAB) ,   KC_A   ,   KC_R   ,   KC_S   ,   KC_T   ,   KC_D   ,   KC_H   ,   KC_N   ,   KC_E   ,   KC_I   ,   KC_O   , KC_DEL,\
-        KC_LSFT ,   KC_Z   ,   KC_X   ,   KC_C   ,   KC_V   ,   KC_B   ,   KC_K   ,   KC_M   ,  MY_COMM ,  MY_DOT  ,TD_SFT_CAP,\
+        KC_LSFT ,   KC_Z   ,   KC_X   ,   KC_C   ,   KC_V   ,   KC_B   ,   KC_K   ,   KC_M   ,  MY_COMM ,  MY_DOT  ,OSM(MOD_LSFT),\
                     KC_LCTL ,  KC_LGUI ,  KC_LALT ,  NUM_SPC ,  MOV_ENT ,  KC_RALT ,  KC_RGUI ,  KC_RCTL \
     ),
 
@@ -57,7 +57,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
  [_QWERTY] =LAYOUT( \
  KC_ESC ,   KC_Q   ,   KC_W   ,   KC_E   ,   KC_R   ,   KC_T   ,   KC_Y   ,   KC_U   ,   KC_I   ,   KC_O   ,   KC_P   , KC_BSPC  ,\
  LT(_SYM,KC_TAB) ,   KC_A   ,   KC_S   ,   KC_D   ,   KC_F   ,   KC_H   ,   KC_G   ,   KC_J   ,   KC_K   ,   KC_L   ,  KC_SCLN , KC_ENT,\
-KC_LSFT ,   KC_Z   ,   KC_X   ,   KC_C   ,   KC_V   ,   KC_B   ,   KC_N   ,   KC_M   ,  MY_COMM ,  MY_DOT  ,TD_SFT_CAP,\
+KC_LSFT ,   KC_Z   ,   KC_X   ,   KC_C   ,   KC_V   ,   KC_B   ,   KC_N   ,   KC_M   ,  MY_COMM ,  MY_DOT  ,OSM(MOD_LSFT),\
             KC_TRNS ,  KC_TRNS ,  KC_TRNS ,  KC_TRNS ,  KC_TRNS ,  KC_TRNS ,  KC_TRNS ,  KC_TRNS \
 ),
 
--- keyboards/lazydesigners/dimple/keymaps/fabian0520/rules.mk
+++ keyboards/lazydesigners/dimple/keymaps/fabian0520/rules.mk
@@ -2,7 +2,7 @@ ifndef QUANTUM_DIR
         include ../../../../Makefile
 endif
 
-TAP_DANCE_ENABLE = yes
+TAP_DANCE_ENABLE = no
 MIDI_ENABLE = no            # MIDI support (+2400 to 4200, depending on config)
 AUDIO_ENABLE = no
 UNICODEMAP_ENABLE = yes
