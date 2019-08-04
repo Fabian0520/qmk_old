c823d70270f7bd41d025d8b927a65d4ff793933c
Fabian Maisberger <fabian.maisberger@googlemail.com>
2019-08-04 12:46:24 +0200

    jd40 und Planck auf OSM umgestellt

--- keyboards/planck/keymaps/fabian0520/config.h
+++ keyboards/planck/keymaps/fabian0520/config.h
@@ -63,9 +63,6 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
 #endif
 
-#define ONESHOT_TAP_TOGGLE 3  /* Tapping this number of times holds the key until tapped this number of times again. */
-#define ONESHOT_TIMEOUT 5000  /* Time (in ms) before the one shot key is released */
-#define TAPPING_TERM 200
 //#define RETRO_TAPPING
 //#define IGNORE_MOD_TAP_INTERRUPT
 
--- keyboards/planck/keymaps/fabian0520/keymap.c
+++ keyboards/planck/keymaps/fabian0520/keymap.c
@@ -24,7 +24,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 [_COLEMAK] =LAYOUT_planck_grid( 
    KC_ESC ,  KC_Q   ,  KC_W   ,   KC_F  ,   KC_P   ,   KC_G   ,   KC_J   ,   KC_L   ,   KC_U   ,   KC_Y   , KC_SCLN  , KC_BSPC ,
    LT(_SYM,KC_TAB)  ,  KC_A   ,  KC_R   ,   KC_S   ,   KC_T   ,   KC_D   ,   KC_H   ,   KC_N   ,   KC_E   ,   KC_I   ,  KC_O    , KC_DEL  ,
-   KC_LSFT,  KC_Z   ,  KC_X   ,   KC_C  ,   KC_V   ,   KC_B   ,   KC_K   ,   KC_M   ,  MY_COMM ,  MY_DOT  , KC_QUOT  , TD_SFT_CAP ,
+   KC_LSFT,  KC_Z   ,  KC_X   ,   KC_C  ,   KC_V   ,   KC_B   ,   KC_K   ,   KC_M   ,  MY_COMM ,  MY_DOT  , KC_QUOT  , OSM(MOD_LSFT) ,
    KC_MUTE,KC_LCTL  , KC_LGUI , KC_LALT ,  NUM_SPC ,  NUM_SPC ,  MOV_ENT ,  MOV_ENT , KC_RALT  ,  KC_RGUI , KC_RCTL  , KC_NO  
  ), 
 
@@ -42,7 +42,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 [_QWERTY] =LAYOUT_planck_grid( 
    KC_ESC ,  KC_Q   ,  KC_W   ,   KC_E   ,   KC_R   ,   KC_T   ,   KC_Y   ,   KC_U   ,   KC_I   ,   KC_O   , KC_P     , KC_BSPC ,
    KC_TAB ,  KC_A   ,  KC_S   ,   KC_D   ,   KC_F   ,   KC_G   ,   KC_H   ,   KC_J   ,   KC_K   ,   KC_L   , KC_SCLN  , KC_ENT  ,
-   KC_LSFT,  KC_Z   ,  KC_X   ,   KC_C   ,   KC_V   ,   KC_B   ,   KC_N   ,   KC_M   ,  MY_COMM ,  MY_DOT  , KC_QUOT  , TD_SFT_CAP ,
+   KC_LSFT,  KC_Z   ,  KC_X   ,   KC_C   ,   KC_V   ,   KC_B   ,   KC_N   ,   KC_M   ,  MY_COMM ,  MY_DOT  , KC_QUOT  , OSM(MOD_LSFT) ,
    KC_NO  ,OSM_CTL  , OSM_GUI , OSM_ALT  ,  NUM_SPC ,  NUM_SPC ,  MOV_ENT ,  MOV_ENT , KC_RALT  ,  KC_RGUI , KC_RCTL  , KC_NO  
  ), 
 
