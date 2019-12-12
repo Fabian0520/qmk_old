d94a3a31b3e9b26ae5a6e54cd58ff7cbdf4cb749
Fabian Maisberger <fabian.maisberger@googlemail.com>
2019-12-12 21:30:23 +0100

    animation funktioniert. rgb_effecs in config geaendert

--- keyboards/crkbd/keymaps/fabian0520/rules.mk
+++ keyboards/crkbd/keymaps/fabian0520/rules.mk
@@ -20,11 +20,12 @@ NKRO_ENABLE                = no # Nkey Rollover - if this doesn't work, see here
 BACKLIGHT_ENABLE           = no  # Enable keyboard backlight functionality
 MIDI_ENABLE                = no  # MIDI controls
 AUDIO_ENABLE               = no  # Audio output on port C6
-UNICODE_ENABLE             = no  # Unicode
+UNICODE_ENABLE             = yes  # Unicode
 BLUETOOTH_ENABLE           = no  # Enable Bluetooth with the Adafruit EZ-Key HID
 RGBLIGHT_ENABLE            = no  # Enable WS2812 RGB underlight.
 SWAP_HANDS_ENABLE          = no  # Enable one-hand typing
 RGB_MATRIX_ENABLE 		   = WS2812
+RGBLIGHT_SPLIT			   = yes
 
 # Do not enable SLEEP_LED_ENABLE. it uses the same timer as BACKLIGHT_ENABLE
 SLEEP_LED_ENABLE = no    # Breathing sleep LED during USB suspend
